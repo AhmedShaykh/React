22d271c7048d8b697f534cad9e81ced3830e8252
Ahmed Saleem Shaikh <98047091+AhmedShaykhETH@users.noreply.github.com>
2022-03-16 22:11:15 +0500

    It's Time To Push !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import NavBar from './Components/NavBar';
+import InfoPanel from './Components/InfoPanel';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>
-        Hello World
-      </h1>
+      <NavBar />
+      <InfoPanel />
     </div>
   );
 }
